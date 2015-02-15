69ddc6f2bf44a04a9ddf3cec2df455b03556d756
songsnow <songsnow@hanmail.net>
2015-02-15 15:51:23 +0900

    TimeComplexity

---
+++ src/choding/TimeComplexity.scala
@@ -0,0 +1,16 @@
+package choding
+
+/**
+ * Created by 송국 on 2015-02-15.
+ */
+object TimeComplexity {
+  def main(args: Array[String]): Unit = {
+    val a = readLine.toInt
+    val b = readLine.toInt
+    println(b)
+    println(sum(b, a))
+  }
+  def sum(a:Int, b:Int):Int = {
+  println(a)
+  a + b}
+}
